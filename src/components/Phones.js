3c141f6ae0c463a690ac70f42ea84a7db4dffa5c
Aierto Francisco Lucero <aiertofranciscolucero@Aiertos-MacBook-Pro.local>
2021-04-25 19:11:38 +0800

    Completed the list of phones page

--- src/components/Phones.js
+++ src/components/Phones.js
@@ -1,103 +1,134 @@
-import React from 'react'
-import { Container, Card, CardColumns } from 'react-bootstrap';
+import React, { Component, useState } from 'react';
+import { Container, Card, CardColumns, Modal, Button, Row, Col, Image, Table } from 'react-bootstrap';
+import ReactModal from 'react-modal';
+import axios from 'axios';
 
-function Phones() {
-    return (
-        <Container className="mt-5">
-            <CardColumns>
-                <Card>
-                    <Card.Img variant="top" src="holder.js/100px160" />
-                    <Card.Body>
-                    <Card.Title>Card title that wraps to a new line</Card.Title>
-                    <Card.Text>
-                        This is a longer card with supporting text below as a natural lead-in to
-                        additional content. This content is a little bit longer.
-                    </Card.Text>
-                    </Card.Body>
-                </Card>
-                <Card className="p-3">
-                    <blockquote className="blockquote mb-0 card-body">
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-                        erat a ante.
-                    </p>
-                    <footer className="blockquote-footer">
-                        <small className="text-muted">
-                        Someone famous in <cite title="Source Title">Source Title</cite>
-                        </small>
-                    </footer>
-                    </blockquote>
-                </Card>
-                <Card>
-                    <Card.Img variant="top" src="holder.js/100px160" />
-                    <Card.Body>
-                    <Card.Title>Card title</Card.Title>
-                    <Card.Text>
-                        This card has supporting text below as a natural lead-in to additional
-                        content.{' '}
-                    </Card.Text>
-                    </Card.Body>
-                    <Card.Footer>
-                    <small className="text-muted">Last updated 3 mins ago</small>
-                    </Card.Footer>
-                </Card>
-                <Card bg="primary" text="white" className="text-center p-3">
-                    <blockquote className="blockquote mb-0 card-body">
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-                        erat a ante.
-                    </p>
-                    <footer className="blockquote-footer">
-                        <small className="text-muted">
-                        Someone famous in <cite title="Source Title">Source Title</cite>
-                        </small>
-                    </footer>
-                    </blockquote>
-                </Card>
-                <Card className="text-center">
-                    <Card.Body>
-                    <Card.Title>Card title</Card.Title>
-                    <Card.Text>
-                        This card has supporting text below as a natural lead-in to additional
-                        content.{' '}
-                    </Card.Text>
-                    <Card.Text>
-                        <small className="text-muted">Last updated 3 mins ago</small>
-                    </Card.Text>
-                    </Card.Body>
-                </Card>
-                <Card>
-                    <Card.Img src="holder.js/100px160" />
-                </Card>
-                <Card className="text-right">
-                    <blockquote className="blockquote mb-0 card-body">
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-                        erat a ante.
-                    </p>
-                    <footer className="blockquote-footer">
-                        <small className="text-muted">
-                        Someone famous in <cite title="Source Title">Source Title</cite>
-                        </small>
-                    </footer>
-                    </blockquote>
-                </Card>
-                <Card>
-                    <Card.Body>
-                    <Card.Title>Card title</Card.Title>
-                    <Card.Text>
-                        This is a wider card with supporting text below as a natural lead-in to
-                        additional content. This card has even longer content than the first to
-                        show that equal height action.
-                    </Card.Text>
-                    <Card.Text>
-                        <small className="text-muted">Last updated 3 mins ago</small>
-                    </Card.Text>
-                    </Card.Body>
-                </Card>
-            </CardColumns>
-        </Container>
-    )
+export default class Phones extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            phones: [],
+            isLoading: true,
+            showModal: false,
+            phoneDetails: ""
+        };
+        this.handleOpenModal = this.handleOpenModal.bind(this);
+        this.handleCloseModal = this.handleCloseModal.bind(this);
+    }
+
+    componentDidMount() {
+        axios.get('https://glacial-refuge-41061.herokuapp.com/api/phones')
+            .then(res => {
+                this.setState({ phones: res.data });
+                this.setState({ isLoading: false });
+            })
+            .catch(function (error) {
+                this.setState({ isLoading: false });
+            })
+    }
+
+    handleOpenModal (d) {
+        this.setState({ phoneDetails: d });
+        this.setState({ showModal: true });
+    }
+    
+    handleCloseModal () {
+        this.setState({ showModal: false });
+    }
+    
+    render() {
+        return (
+            <Container className="mt-5">
+                <div class={!this.state.isLoading ? 'd-none' : 'd-flex justify-content-center'}>
+                    <div class="spinner-border " id="page-spinner" role="status">
+                        <span class="sr-only d-flex justify-content-center">Loading...</span>
+                    </div>
+                </div>
+                <CardColumns>
+                    {
+                        this.state.phones.map(
+                            d => (
+                                <Card key={d.id} value={d} onClick={() => this.handleOpenModal(d)}>
+                                    <Card.Img className="p-3" variant="top" src={d.imageFileName} />
+                                    <Card.Body>
+                                    <Card.Title>
+                                        <p class="d-flex">
+                                            <span>{d.name}</span>
+                                            <span class="ml-auto badge badge-danger">${d.price}</span>
+                                        </p>
+                                    </Card.Title>
+                                    <Card.Text>
+                                        {d.description}
+                                    </Card.Text>
+                                    </Card.Body>
+                                </Card>
+                            )
+                        )
+                    } 
+                </CardColumns>
+                
+                <Container id="modalContainer">
+                   
+                </Container>
+
+
+                <ReactModal isOpen={this.state.showModal} parentSelector={() => document.querySelector('#modalContainer')}>
+                    <Row>
+                        {this.state.phoneDetails == ""
+                            ?   <div class="spinner-border" id="page-spinner" role="status">
+                                    <span class="sr-only">Loading...</span>
+                                </div>
+                            :   <Container>
+                                    <Modal.Body>
+                                        <Row>
+                                            <Col sm={4}>
+                                                <Image className="w-100" src={this.state.phoneDetails.imageFileName} rounded />
+                                            </Col>
+                                            <Col sm={8}>
+                                                <h1>{this.state.phoneDetails.name}</h1>
+                                                <p>{this.state.phoneDetails.description}</p>
+                                                <Table striped bordered hover size="sm">
+                                                    <tbody>
+                                                        <tr>
+                                                            <th>Manufacturer</th>
+                                                            <td>{this.state.phoneDetails.manufacturer}</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <th>Color</th>
+                                                            <td>{this.state.phoneDetails.color}</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <th>Screen</th>
+                                                            <td>{this.state.phoneDetails.screen}</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <th>Processor</th>
+                                                            <td>{this.state.phoneDetails.processor}</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <th>Ram</th>
+                                                            <td>{this.state.phoneDetails.ram}</td>
+                                                        </tr>
+                                                        <tr className="bg-danger text-white">
+                                                            <th>Price</th>
+                                                            <td>{this.state.phoneDetails.price}</td>
+                                                        </tr>
+                                                    </tbody>
+                                                </Table>
+                                            </Col>
+                                        </Row>
+                                    </Modal.Body>
+                                    <Modal.Footer>
+                                        <Button variant="secondary" onClick={this.handleCloseModal}>
+                                            Close
+                                        </Button>
+                                    </Modal.Footer>
+                                </Container>
+                                
+                        }
+                    </Row>
+                </ReactModal>
+            </Container>
+        )
+    }
 }
-
-export default Phones
